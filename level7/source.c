00eadd501e89fc0f184b4d2051fd88a71b8b9d34
olesgedz <gedzoles@gmail.com>
2022-01-26 00:12:02 +0300

    smth

--- level7/source.c
+++ level7/source.c
@@ -8,7 +8,7 @@ void	m()
 	printf(s____d, c, time());
 }
 
-int	main(int ac, char **av)
+int	main(int ac, char **argv)
 {
 	char	*pass = "/home/user/level8/.pass";
 	int	*save1;
@@ -22,8 +22,8 @@ int	main(int ac, char **av)
 	save2[0] = 2;
 	save2[1] = malloc(8);
 	
-	strcpy(save1[1], av[1]);
-	strcpy(save2[1], av[2]);
+	strcpy(save1[1], argv[1]);
+	strcpy(save2[1], argv[2]);
 	fgets(c, 68, fopen(pass, 0x80486e9));
 	puts("~~");
 	return (0);
--- main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main() {
-
-     if ((0xb0000000 & 0xb0000000) == 0xb0000000) {
-         std::cout << "FAIL" << std::endl;
-    }
-}
